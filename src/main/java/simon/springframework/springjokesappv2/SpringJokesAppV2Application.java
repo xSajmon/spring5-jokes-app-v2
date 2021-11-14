d544c7e8d98bd0af16ee6cd1ae764b8da73c79f2
simon <szymon.stypka@o2.pl>
2021-11-14 23:58:52 +0100

    Initial Project

---
+++ src/main/java/simon/springframework/springjokesappv2/SpringJokesAppV2Application.java
@@ -0,0 +1,13 @@
+package simon.springframework.springjokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokesAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/simon/springframework/springjokesappv2/SpringJokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package simon.springframework.springjokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
